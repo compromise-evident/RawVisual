d0097aef59e78b09b6310a7192d932109ba29052
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2024-01-24 18:59:47 -0700

    cpp: comments: tesseract whitelist

--- RawVisual.cpp
+++ RawVisual.cpp
@@ -1162,7 +1162,7 @@ shotwell                 GUI  Image viewer+editor with edit options right on the
 vlc                      GUI  Universal media interface (good for music & video, safely plays corrupted and partly-downloaded video, plays streams from online source.)
 xz-utils                *CLI  Compress/decompress .xz files (especially downloaded OS images for Raspberry Pi.) Command: xz -d -v /path/to/file.
 
-tesseract-ocr           *CLI  Recognize text from image. Use: tesseract image.jpg my_words
+tesseract-ocr           *CLI  Recognize text from image. Use: tesseract image.jpg my_words   (To make it look for specific char hence improve accuracy & efficiency, use: tesseract image.png output.txt --tessedit_char_whitelist=0123456789RSTXYZ)
 qrencode                *CLI  Make qr code.              Use: qrencode -o output.png "my text, no ! char."
 zbar-tools              *CLI  Read qr code.              Use: zbarimg image.png
 xdg-open                *CLI  Open image.                Use: xdg-open your_image_file.jpg && sleep 5 && pkill -f xdg-open   (Comes pre-installed.)
