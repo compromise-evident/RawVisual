54f4a36ded90df0db2a803dae28b155d7f1cb130
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2024-09-25 23:02:58 -0600

    Add files via upload

--- RawVisual.cpp
+++ RawVisual.cpp
@@ -480,9 +480,8 @@ qrencode                *CLI  Make qr code.               Use: qrencode -o outpu
 zbar-tools              *CLI  Read qr code.               Use: zbarimg image.png
 xdg-open                *CLI  Open image.                 Use: xdg-open your_image_file.jpg && sleep 5 && pkill -f xdg-open   (Comes pre-installed.)
 pkill                   *CLI  Close image.                Use: pkill -f "eog your_image_file.jpg"                             (Comes pre-installed.)
-libgmp-dev              *LIB  GMP - GNU Multiple Precision Arithmetic Library. Run it: "apt install g++ geany libgmp-dev".  Open the .cpp in Geany. Append "-lgmp"  to Geany's compile & build commands. Hit F9 once. F5 to run. Don't forget to add "#include <gmp.h>"      in that .cpp file!
-libdlib-dev             *LIB  dlib - C++ open-source Machine Learning library. Run it: "apt install g++ geany libdlib-dev". Open the .cpp in Geany. Append "-ldlib" to Geany's compile & build commands. Hit F9 once. F5 to run. Don't forget to add "#include <dlib/dnn.h>" in that .cpp file!
-python3-torch           *LIB  PyTorch - META's python-only open-source Machine Learning library. Run it: "apt install python3-torch geany". Open the .py in Geany. Replace "python" with "python3" in Geany's execute command. F5 to run.
+libgmp-dev              *LIB  GMP - GNU Multiple Precision Arithmetic Library. Run it: "apt install g++ geany libgmp-dev".  Open the .cpp in Geany. Append "-lgmp"  to Geany's compile & build commands. Hit F9 once. F5 to run. Don't forget to add "#include <gmp.h>" in that .cpp file!
+python3-torch           *LIB  PyTorch - Machine Learning library practical only in Python. Run it: "apt install python3-torch geany". Open the .py in Geany. Replace "python" with "python3" in Geany's execute command. F5 to run.
 
 Commands:
 apt-mark hold package_name     (stop updates to this package)
