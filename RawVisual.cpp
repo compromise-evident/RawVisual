22c3c232b3056084f5c40370074277a4db04a26f
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2025-11-06 22:48:01 -0700

    How to fix audio in Devuan 6.
    Add command to Startup Applications: sh -c "pipewire & wireplumber & pipewire-pulse & wait"
    
    Previously, the line above was "apt install pulseaudio".
    But pulseaudio is the work of Lennart Poettering--
    the author of an obvious backdoor called systemd.
    
    This fix works for MATE. Devuan 6 MATE comes
    with pipewire, wireplumber, and pipewire-pulse.
    IMPORTANT: the order of those 3 packages
    to be started, is important. You're welcome.
    (Devuan 6 live does not have this problem.)

--- RawVisual.cpp
+++ RawVisual.cpp
@@ -564,11 +564,11 @@ Get length of .wav file in seconds using sox:    system("sox my_audio.wav -n sta
 Play audio file through vlc then close vlc:      system("vlc my_audio.wav --play-and-exit 2>/dev/null &");
 Set system volume using pre-installed ALSA:      system("amixer -q set Master 75%");
 
-Personal Devuan install:
-If no sound: apt install pulseaudio
-apt install g++ geany geany-plugin-automark geany-plugin-spellcheck gparted lightdm-settings mate-tweak shotwell vlc
-No 5-second countdown: log in as root, change 5 to 0 in file "/etc/default/grub", then do "update-grub".
-If MacBook Pro: cut speaker wires to eliminate chime, physically swap keyboard keys "fn" and "left Ctrl", log in as root, create file "/etc/modprobe.d/hid_apple.conf" with content "options hid_apple swap_fn_leftctrl=1 fnmode=2", then do "sudo update-initramfs -u -k all".   */
+My Devuan:
+Packages I need:  apt install g++ geany geany-plugin-automark geany-plugin-spellcheck gparted lightdm-settings mate-tweak shotwell vlc
+If sound broken:  add command to Startup Applications: sh -c "pipewire & wireplumber & pipewire-pulse & wait"
+If MacBook Pro :  cut speaker wires to eliminate chime, physically swap keyboard keys "fn" and "left Ctrl", log in as root, create file "/etc/modprobe.d/hid_apple.conf" with content "options hid_apple swap_fn_leftctrl=1 fnmode=2", then do "sudo update-initramfs -u -k all"
+No 5s countdown:  log in as root, change 5 to 0 in file "/etc/default/grub", then do "update-grub"   */
 
 
 
