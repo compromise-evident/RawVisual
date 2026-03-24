8a5da74a67cfb1174c42c8422c91a3773a974f9a
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2026-03-23 21:16:25 -0600

    Version 7.0.1 - path[] is now a string.

--- RawVisual.cpp
+++ RawVisual.cpp
@@ -1,4 +1,4 @@
-/*Version 7.0.0                                                                 Run it: "apt install g++ geany". Open the .cpp in Geany. Hit F9 once. F5 to run.
+/*Version 7.0.1                                                                 Run it: "apt install g++ geany". Open the .cpp in Geany. Hit F9 once. F5 to run.
 Creates a minimalist analysis file about your file.
 Occurrence, raw, text. This cpp contains resources;
 it should be the first tab in your editor. */
@@ -13,11 +13,9 @@ int main()
 	ofstream out_stream;
 	
 	//Gets path, fixes it if dropped.
-	cout << "\nDrop/enter file:\n";
-	char path[100000] = {'\0'}; cin.getline(path, 100000); if(path[0] == '\0') {cin.getline(path, 100000);}
-	if(path[0] == '\'') {for(int bm = 0, a = 0; a < 100000; a++) {if(path[a] != '\'') {path[bm] = path[a]; if(path[bm] == '\\') {path[bm] = '\'';} bm++;}}}
-	for(int a = 99999; a >= 0; a--) {if(path[a] != '\0') {if(path[a] == ' ') {path[a] = '\0';} break;}}
-	in_stream.open(path); if(in_stream.fail() == true) {cout << "\nNo path " << path << "\n"; return 1;} in_stream.close();
+	cout << "\nDrop/enter file:\n"; string path; getline(cin, path); if(path[0] == '\0') {getline(cin, path);}
+	if(path[0] == '\'') {path.erase(0, 1); path.pop_back(); path.pop_back();}
+	in_stream.open(path); if(in_stream.fail()) {cout << "\nNo path " << path << "\n"; return 1;} in_stream.close();
 	
 	//Gets byte occurrence.
 	in_stream.open(path); in_stream.get(file_byte); long long byte_occur[256] = {0};
